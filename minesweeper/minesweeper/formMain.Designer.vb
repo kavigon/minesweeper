501b5b168b7434a5d6bdfbe68dd2253a6a5be37e
kavigon <jskavigon@gmail.com>
2016-09-04 17:56:50 +0900

    Add files via upload

---
+++ minesweeper/minesweeper/formMain.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class formMain
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.panelButton = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'panelButton
+        '
+        Me.panelButton.Location = New System.Drawing.Point(12, 12)
+        Me.panelButton.Name = "panelButton"
+        Me.panelButton.Size = New System.Drawing.Size(390, 390)
+        Me.panelButton.TabIndex = 0
+        '
+        'formMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(417, 413)
+        Me.Controls.Add(Me.panelButton)
+        Me.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "formMain"
+        Me.Text = "Minesweeper"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents panelButton As Panel
+End Class
